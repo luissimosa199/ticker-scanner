68138874c86719f900c94788a5375324cf53112c
Luis <simosa37@gmail.com>
2024-08-03 10:59:05 -0300

    getHtmlMockFilePath improved for dev env

--- src/tickets/mocks/htmlMock.ts
+++ src/tickets/mocks/htmlMock.ts
@@ -1,7 +1,12 @@
 import * as fs from 'fs';
 import * as path from 'path';
 
-export const htmlMock = fs.readFileSync(
-  path.join(__dirname, '../../utilities/ticket-parser/parsers/htmlMock.html'),
-  'utf-8',
-);
+export function getHtmlMockFilePath(): string {
+  const baseDir =
+    process.env.NODE_ENV === 'production'
+      ? path.join(__dirname, '../../..')
+      : path.join(__dirname, '../../../src');
+  return path.join(baseDir, 'utilities/ticket-parser/parsers/htmlMock.html');
+}
+
+export const htmlMock = fs.readFileSync(getHtmlMockFilePath(), 'utf-8');
