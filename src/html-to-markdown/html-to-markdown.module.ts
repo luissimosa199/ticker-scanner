594d93eba3cc5467d4b30fbc56d7a4ff618c8ee5
Luis <simosa37@gmail.com>
2024-08-03 09:46:58 -0300

    html-to-markdown basic functionality implemented

--- src/html-to-markdown/html-to-markdown.module.ts
+++ src/html-to-markdown/html-to-markdown.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { HtmlToMarkdownService } from './html-to-markdown.service';
 
 @Module({
-  providers: [HtmlToMarkdownService]
+  providers: [HtmlToMarkdownService],
+  exports: [HtmlToMarkdownService],
 })
 export class HtmlToMarkdownModule {}
--- src/html-to-markdown/html-to-markdown.service.ts
+++ src/html-to-markdown/html-to-markdown.service.ts
@@ -1,4 +1,15 @@
 import { Injectable } from '@nestjs/common';
+import TurndownService from 'turndown';
 
 @Injectable()
-export class HtmlToMarkdownService {}
+export class HtmlToMarkdownService {
+  private turndownService: TurndownService;
+
+  constructor() {
+    this.turndownService = new TurndownService();
+  }
+
+  convertHtmlToMarkdown(html: string): string {
+    return this.turndownService.turndown(html);
+  }
+}
